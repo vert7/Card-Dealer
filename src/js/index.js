57b02525fb8d07e0c059ccbdafcfefe8de6ae10d
vert7 <evertponce@outlook.com>
2019-10-03 19:01:03 +0000

    Card Dealer

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,41 @@ import "../assets/img/4geeks.ico";
 //import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document
+    .querySelector(".card", ".card1", ".card2", ".card3")
+    .classList.add(generateRandomSuit());
+  document.querySelector(
+    ".card",
+    ".card1",
+    ".card2",
+    ".card3"
+  ).innerHTML = generateRandomNumbers();
+};
+
+let generateRandomNumbers = () => {
+  let numbers = [
+    "A",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "K",
+    "Q"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
+
+let generateRandomSuit = () => {
+  let suit = ["spade", "heart", "diamond", "club"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit];
 };
